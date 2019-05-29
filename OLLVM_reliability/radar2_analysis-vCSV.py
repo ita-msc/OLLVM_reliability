2386a311340468debbba6d5aa05f67a8c428fac3
Anais NALEM <anais.nalem@laposte.net>
2019-05-29 15:15:26 +0200

    From json to specific csv format conversion ok but 'not 'elegant'

--- OLLVM_reliability/radar2_analysis-vCSV.py
+++ OLLVM_reliability/radar2_analysis-vCSV.py
@@ -2,8 +2,6 @@ import sys
 import r2pipe
 import json
 import os
-from itertools import repeat
-import math
 
 # ------------------------------------------------------------------------------------------------------------------------------------
 # This script intends to decide, thanks to the output data it generates, which sorting criterion to use in the radar2_analysis-v2.py 
@@ -13,14 +11,11 @@ import math
 # Choose the analysis and listing commands you want to apply to the input file
 ANALYSIS_CMD        = "aa"
 LISTING_CMD         = "aflj~{}"
-PERCENTAGE          = 0.3
-SORTING_CRITERION   = "nbbs"        # size, nbbs, ratio
 #######################################################################################################################
 
 
 
 # opening the input executable file
-print("Opening the input executable file")
 r2 = r2pipe.open(str(sys.argv[1])) 
 
 
@@ -35,37 +30,37 @@ aflj_output = r2.cmd(LISTING_CMD)
 print("Loading and processing the json output...")
 aflj_json           = json.loads(aflj_output)
 aflj_lines_number   = len(aflj_json)
-complete_aflj_data_array = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] for i in repeat(None, aflj_lines_number)]
 
+columnTitleRow = "programName,calltype, realsz, diff, name, cc, indegree, nargs, difftype, edges, outdegree, cost, nlocals, offset, ebbs, nbbs, type, size, datarefs, cref_c, cref_j\n"
+parameters = ["calltype", "realsz", "diff", "name", "cc", "indegree", "nargs", "difftype", "edges", "outdegree", "nlocals", "cost", "offset", "ebbs", "nbbs", "type", "size"]
 
-
-"""
-    STEP 1 => Building complete_aflj_data_array array
-    :structure: array of arrays:
-                [
-                    [offset_0, nbbs_0, size_0],
-                    [offset_1, nbbs_1, size_1],
-                    ...
-                    [offset_n, nbbs_n, size_n]
-                ]
-    :size: n = number of output lines of the aflj r2 command
-"""
-for line in range(aflj_lines_number):
-    complete_aflj_data_array[line][0]=aflj_json[line]['name']
-    complete_aflj_data_array[line][1]=aflj_json[line]['offset']
-    complete_aflj_data_array[line][2] = aflj_json[line]['nbbs']
-    complete_aflj_data_array[line][3] = aflj_json[line]['size']
-    complete_aflj_data_array[line][4]=aflj_json[line]['calltype']
-    complete_aflj_data_array[line][5]=aflj_json[line]['realsz']
-    complete_aflj_data_array[line][6]=aflj_json[line]['diff']
-    complete_aflj_data_array[line][7]=aflj_json[line]['cc']
-    complete_aflj_data_array[line][8]=aflj_json[line]['indegree']
-    complete_aflj_data_array[line][9]=aflj_json[line]['args']
-    complete_aflj_data_array[line][10]=aflj_json[line]['difftype']
-    complete_aflj_data_array[line][11]=aflj_json[line]['edges']
-    complete_aflj_data_array[line][12]=aflj_json[line]['outdegree']
-    complete_aflj_data_array[line][13]=aflj_json[line]['cost']
-    complete_aflj_data_array[line][14]=aflj_json[line]['nlocals']
-    complete_aflj_data_array[line][15]=aflj_json[line]['ebbs']
-    complete_aflj_data_array[line][16]=aflj_json[line]['type']
-  
+for output in aflj_json:
+    row1 = str(sys.argv[1]) + ","
+    row2 = ""
+    for element in output:
+        J_count = 0
+        C_count = 0
+        if (element in parameters):
+        # just print it properly
+            row1+= str(output[element]) + ","
+        else:
+            if (element == "callrefs"):
+            # count the number of J and C
+                if (len(output[element]) == 0): # if there is no callref
+                    row2+= "N\A,N\A,"
+                else: # if there is at least one callref
+                    for cref in range(0, len(output[element])):
+                    # for each {type: type_value, addr: addr_value, at: at_value}
+                        if ((output[element][cref]['type']) == 'J'):
+                            J_count+=1
+                        else: 
+                            C_count+=1
+                    row2+= str(C_count) + "," + str(J_count) + ","
+            if (element == "datarefs"):
+            # count the number of elements in the datarefs list
+                if (len(output[element]) == 0): # if there is no dataref
+                    row2+= "nodataref,"
+                else:
+                    row2+=str(len(output[element])) + ","
+    print (columnTitleRow)
+    print (row1 + row2)
