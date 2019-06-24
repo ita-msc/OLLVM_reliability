c9900333c9e6f767d66188453c760a83faa8ee9c
ANAIS NALEM <anais.nalem@atos.net>
2019-06-24 11:53:02 +0200

    changed 'skimming' into 'filtering'

--- OLLVM_reliability/radare2Analysis_&_OutputFiltering-v2.py
+++ OLLVM_reliability/radare2Analysis_&_OutputFiltering-v2.py
@@ -29,30 +29,30 @@ radare2_output_lines_number   = len(radare2_output_json)
 
 
 """
-    Radare2 output skimming
+    Radare2 output filtering
     The purpose of this protocol is to remove the least useful and relevant functions from the program and thus save the best of them.
-    A "skimming formula" is applied to each function. Those with a score of 0 are automatically expelled from the sample of 
+    A "filtering formula" is applied to each function. Those with a score of 0 are automatically expelled from the sample of 
     individuals who will be involved in setting the final score.
 
-    Skimming_formula = (realsize + nbbs + realsize/nbbs) * number_of_jumps
+    filtering_formula = (realsize + nbbs + realsize/nbbs) * number_of_jumps
 """
 radare2_ready_to_be_scored = []
 
-# applying the "skimming formula" to score each function of the program
+# applying the "filtering formula" to score each function of the program
 for line in radare2_output_json:
     fcn_score = 0
     if not 'callrefs' in line.keys() or not line['callrefs']:
         fcn_score = 0
-        line['skimming_score'] = fcn_score
+        line['filtering_score'] = fcn_score
     else:
         J_count = sum(cref['type']=="J" for cref in line['callrefs'])
         fcn_score = (line['nbbs'] + line['realsz'] + (line['realsz']/line['nbbs'])) * J_count
-        line['skimming_score'] = fcn_score
+        line['filtering_score'] = fcn_score
 
 
-# building the "ready-to-be-scored" list where each element has "skimming_score" > 0
+# building the "ready-to-be-scored" list where each element has "filtering_score" > 0
 for line in radare2_output_json:
-    if line['skimming_score'] > 0.0 : 
+    if line['filtering_score'] > 0.0 : 
         radare2_ready_to_be_scored.append(line)
 
 print (radare2_ready_to_be_scored)
