05aa55e0f94818de90ec4c2e53e32a77bfe63cb4
ANAIS NALEM <anais.nalem@atos.net>
2019-07-17 16:49:31 +0200

    Scoring algorithm finalized

--- OLLVM_reliability/scoring.py
+++ OLLVM_reliability/scoring.py
@@ -2,13 +2,14 @@
 import sys 
 import csv
 
-csv_reader = csv.reader(open(str(sys.argv[1]),mode='r'), delimiter=',')
+# Scores initialization:
 obfuscated_score = 0.0
 non_obfuscated_score = 0.0
-size = len(list(csv.reader(open(str(sys.argv[1])))))-1  # total of both the obfuscated and non obfuscated programs (minus 1, to exclude the name of the columns' row)
-    
-next(csv_reader)
 
+
+
+csv_reader = csv.reader(open(str(sys.argv[1]),mode='r'), delimiter=',')
+next(csv_reader)
 for row in csv_reader:
     if "fla" in row[0] or "sub" in row[0] or "bcf" in row[0] or "strobf" in row[0]:   # if the name of the program contains anything related to obfuscation
         obfuscated_score+=float(row[1])
@@ -16,12 +17,13 @@ for row in csv_reader:
         non_obfuscated_score+=float(row[1])
 
 
+# Calculate the average score for the obfuscated et non obfuscated programs:
+size = len(list(csv.reader(open(str(sys.argv[1])))))-1  # total of both the obfuscated and non obfuscated programs (minus 1, to exclude the name of the columns' row
+obfuscated_score = obfuscated_score/(size/2)
+non_obfuscated_score = non_obfuscated_score/(size/2)
 
-print(obfuscated_score/(size/2))
-print(non_obfuscated_score/(size/2))
 
-csv_writer = csv.writer(open(str(sys.argv[1]),mode=''), delimiter=',')
-csv_writer.writerow('coucou')
-# csv.writer("\n\n"+ "Obfuscated final score,"+ str(obfuscated_score))
-# csv_reader.write("Non obfuscated final score,"+ str(non_obfuscated_score))
-
+# Write these two scores at the end of the csv formatted scoring results file:
+csv_writer = csv.writer(open(str(sys.argv[1]),mode='a'), delimiter=',')
+csv_writer.writerow(["Obfuscated final score", str(obfuscated_score)])
+csv_writer.writerow(["Non obfuscated final score", str(non_obfuscated_score)])
